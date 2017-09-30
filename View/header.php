8bc510f1d596753e3e6a8c03e68800c12212b7fe
Toumi Wajdi <toumi.wajdiat@gmail.com>
2017-09-30 16:17:19 +0100

    header msg

--- View/header.php
+++ View/header.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ASUS
- * Date: 30/09/2017
- * Time: 15:44
- */
+<html>
+<head>
+    <meta charset="UTF-8" name="Smartguard" />
+    <link href="../Src/CSS/materialize.min.css" rel="stylesheet" type="text/css"/>
+    <link href="../Src/CSS/style.css" rel="stylesheet" type="text/css"/>
+    <title>Smart Guard Administrateur : Solution Dispatching Ambulance</title>
+</head>
