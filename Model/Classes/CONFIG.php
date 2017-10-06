76ed4bc753f4869b8accfbf60528bb4163064657
Toumi Wajdi <toumi.wajdiat@gmail.com>
2017-10-06 17:26:20 +0100

    Config correction

--- Model/Classes/CONFIG.php
+++ Model/Classes/CONFIG.php
@@ -2,14 +2,19 @@
 /**
  * Created by PhpStorm.
  * User: ASUS
- * Date: 30/09/2017
- * Time: 15:41
+ * Date: 06/10/2017
+ * Time: 17:25
  */
-function getConnexion()
-{$db="mysql:host=localhost;dbname=dmse";
-    $user="root";
-    $pw="";
-    $connexion=new PDO($db,$user,$pw);
-    return $connexion;
+
+class CONFIG
+{
+    function getConnexion()
+    {   $db="mysql:host=localhost;dbname=dmse";
+        $user="root";
+        $pw="";
+        $connexion=new PDO($db,$user,$pw);
+        return $connexion;
+
+    }
 
 }
