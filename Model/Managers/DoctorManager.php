6b802339b7633fa96fc5c1c9bf4b4f44a979c6de
waeltek <32436531+waeltek@users.noreply.github.com>
2017-10-07 15:46:29 +0100

    Doctor Manger Update

--- Model/Managers/DoctorManager.php
+++ Model/Managers/DoctorManager.php
@@ -5,7 +5,7 @@
  * Date: 05/10/2017
  * Time: 14:06
  */
-require_once "CONFIGMANAGER.php";
+include_once path . "Model/Classes/CONFIG.php";
 
 
 class DoctorManager
@@ -14,12 +14,13 @@ class DoctorManager
 
     public function __construct()
     {
-        $this->cnx=new CONFIGMANAGER();
+        $obj = new CONFIG();
+        $this->cnx = $obj->getConnexion();
     }
 
     public function createTable()
     {
-        $query="";
+        $query="CREATE TABLE `SmartGuard`.`person` ( `id` INT(6) NOT NULL , `firstname` VARCHAR(20) NOT NULL , `lastname` VARCHAR(20) NOT NULL , `phone` INT(10) NOT NULL , `mail` VARCHAR(50) NOT NULL , `type` VARCHAR(18) NOT NULL , `matricule` VARCHAR(20) NOT NULL , PRIMARY KEY (`id`)) ENGINE = MyISAM;";
         $res=$this->cnx->exec($query);
         return $res;
 
@@ -27,6 +28,9 @@ class DoctorManager
 
     public function addDoctor($doctor)
     {
+        $query="INSERT INTO person VALUE ('".$doctor->getId()."','".$doctor->getFirstname()."','".$doctor->getLastname()."','".$doctor->getPhone()."','".$doctor->getMail()."','doctor','".$doctor->getMatricule()."')";
+        $res=$this->cnx->exec($query);
+        return $res;
 
 
     }
@@ -43,20 +47,25 @@ class DoctorManager
 
     public function getDoctorByMatricule($matricule)
     {
-        $query="Select * from doctor WHERE matricule=$matricule";
+        $query="Select * from person WHERE matricule='$matricule'";
+        var_dump($query);
+        $res=$this->cnx->query($query);
+        return $res->fetch();
 
     }
 
 
     public function deleteDoctorByMatricule($matricule)
     {
-        $query="DELETE FROM doctor WHERE matricule=".$matricule;
+        $query="DELETE FROM person WHERE matricule='".$matricule."'";
+        $res=$this->cnx->exec($query);
+        return $res;
 
     }
 
     public function getAll()
     {
-        $query="SELECT * FROM doctor";
+        $query="SELECT * FROM person";
         $res=$this->cnx->query($query);
         return $res->fetechAll($res);
 
