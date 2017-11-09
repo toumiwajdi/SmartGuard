5b40e3ee2faf6e41ce6189e1ad34acb86cfbd316
Toumi Wajdi <toumi.wajdiat@gmail.com>
2017-11-09 13:44:52 +0100

    Adding APi login

---
+++ Model/Api/login.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 01/11/2017
+ * Time: 21:39
+ */
+
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+    require_once $_SERVER["DOCUMENT_ROOT"].'/SmartGuard/config.php';
+    require_once path . 'Model/Managers/SecourAgentManager.php';
+    getRescueLogin();
+}
+function getRescueLogin(){
+    $login=$_POST["login"];
+    $password=$_POST["password"];
+    $res=new SecourAgentManager();
+    $secure=$res->login($login,$password);
+    header("Content-Type:application/json");
+    if ($secure[0])
+    {
+        if ($secure[9]==1){
+            $resultat['sucess']=$secure;
+            echo json_encode($resultat);
+        }
+        else
+        {
+            $resultat['inactive']="Account not active yet please contact smartguard@help.tn";
+            echo json_encode($resultat);
+        }
+
+    }
+    else{
+        $resultat['error']="In valid username or password";
+        echo json_encode($resultat);
+    }
+}
